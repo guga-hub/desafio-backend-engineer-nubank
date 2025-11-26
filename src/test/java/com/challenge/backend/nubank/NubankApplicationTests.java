12f322f6ce3ae04b92cd48c996d8ef8d73c9edad
gugaHub <lima.universitario@gmail.com>
2025-11-26 11:43:38 -0300

    Initialize Spring Boot application with Maven setup and Docker configuration

---
+++ src/test/java/com/challenge/backend/nubank/NubankApplicationTests.java
@@ -0,0 +1,8 @@
+package com.challenge.backend.nubank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubankApplicationTests {
+}
